00d8a60c88b633486bd25efa69fb502bc172c51e
Harris Sontanu <harris.sontanu@gmail.com>
2026-05-14 09:07:59 +0800

    refactor: implement persistent volume storage for public and storage directories in staging environment

--- docker-startup.staging.sh
+++ docker-startup.staging.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+# Sync public files (including Vite-built assets) to shared volume
+cp -rT /app/public-docker /app/public
+
 php artisan key:generate --force
 php artisan migrate --force
 php artisan storage:link
+
 php-fpm
