bb68ccf876d8b5e85cc5c9de2493a4e5902378b6
Harris Sontanu <harris.sontanu@gmail.com>
2026-05-14 09:24:57 +0800

    fix: permissions for php-fpm in staging

--- docker-startup.staging.sh
+++ docker-startup.staging.sh
@@ -3,6 +3,10 @@
 # Sync public files (including Vite-built assets) to shared volume
 cp -rT /app/public-docker /app/public
 
+# Fix permissions for PHP-FPM
+chown -R www-data:www-data /app/public /app/storage /app/bootstrap/cache
+chmod -R 775 /app/storage /app/bootstrap/cache
+
 php artisan key:generate --force
 php artisan migrate --force
 php artisan storage:link
